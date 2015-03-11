152e2a7984586cd85bbf738adc56b2a4e8ca013d
Jort van Breda <jort@onlinetouch.nl>
2015-03-11 16:25:02 +0100

    Initial commit Spinner library to spin texts or keyword.
    Usage:
    %keyword% => refers to a spinner called keyword.

---
+++ src/spinner/core.clj
@@ -0,0 +1,164 @@
+(ns spinner.core
+  (:require [clojure.java.io   :as io]
+            [clojure-csv.core  :as csv])
+  (:import java.util.regex.Pattern java.io.File))
+
+(def spinners (atom {}))
+(def ^:dynamic spinner-ns nil)
+
+(def percent-regex #"\"|\\%|(?<!\\)%[^%\s]+%")
+
+(defn percent-clojurizer [match]
+  (cond
+    (= match "\"")
+    "\\\""
+    (= match "\\%")
+    "%"
+
+    (= \% (first match))
+    (str "\" " (subs match 1 (dec (count match))) "\"") ; TODO: Allow to be namespace independent
+
+    :else match))
+
+(defn parse-str [string]
+  (str "[\"" (clojure.string/replace string percent-regex percent-clojurizer) "\"]"))
+
+(defn- resolve-symbolé [s]
+  (if (string? s)
+    s
+  ;else
+    (for [% s]
+      (if (string? %) %
+      ;else
+        (if-let [fn (resolve %)] (fn) (throw (IllegalArgumentException. (str "Var not found: %" % "%"))))))))
+
+(defn compile-str [string]
+  (let [[head & tail :as parsed]
+        (->>  (parse-str string)
+              (read-string)
+              (remove #(= "" %)))]
+    (if (and (nil? tail) (string? (first parsed)))
+      head
+    ;else
+      (resolve-symbolé parsed))))
+
+(defn- apply-str [result]
+  (if (string? result)
+    result
+    (apply str (map #(if (string? %) % (%)) result))))
+
+(defn- fn-expl [fn] (re-matches #"(.+?)(?:\.([^.]*$))?" (.getName fn)))
+(defn- extension [fn]
+  (last (fn-expl fn)))
+
+(defn- basename [fn]
+  (second (fn-expl fn)))
+
+; Multi method
+(defmulti keyword->fnc (fn [spinner-name values fnc]
+  (type values)))
+
+; Vector
+(defmethod keyword->fnc clojure.lang.PersistentVector [spinner-name values fnc]
+  (let [values (mapv compile-str values)]
+    #(apply-str (fnc values))))
+
+; Map
+(defmethod keyword->fnc clojure.lang.PersistentArrayMap [spinner-name values fnc]
+  (let [[spin s-keys] (first values)
+        spin-fn       (intern spinner-ns (symbol spin))
+        spin-dynamic  (intern spinner-ns (symbol (str "*" spin "*")) nil)]
+        (.setDynamic spin-dynamic true)
+        (doseq [[kolom-naam kolom-values] (rest values)]
+          (intern spinner-ns (symbol (str spin "->" kolom-naam))
+          (let [kolom-values (zipmap s-keys (map compile-str kolom-values))]
+            #(apply-str (kolom-values (or (var-get spin-dynamic) (spin-fn)))))))
+      (keyword->fnc spinner-name s-keys fnc)))
+
+; File
+(defmulti from-file (fn [spinner-name file fnc]
+  (extension file)))
+
+(defmethod from-file :default [spinner-name file fnc]
+  (let [content
+    (with-open [csv-file (io/reader file)]
+      (let [[columns & data]  (->> (csv/parse-csv csv-file) doall)
+            columns           (cons (str spinner-name) (rest columns))]
+        (loop [c columns i 0 result (transient (array-map))]
+          (if-not (first c)
+            (persistent! result)
+            (let [result (assoc! result (first c) (into [] (map #(nth % i) data)))]
+              (recur (next c) (inc i) result))))))]
+  (keyword->fnc spinner-name content fnc)))
+
+(defmethod keyword->fnc java.io.File [spinner-name values fnc]
+  (from-file spinner-name values fnc))
+
+; :default
+(defmethod keyword->fnc :default [spinner-name values fnc]
+  (prn "Could not handle spinner-name" spinner-name) values)
+
+; TODO
+; spinner-name kan namespace bevatten deze binden als dat zo is.
+(defn defspin* [spinner-name values fnc]
+  (binding [spinner-ns
+            (cond
+             (and (symbol? spinner-ns) (find-ns spinner-ns))
+             (find-ns spinner-ns)
+
+             (and (symbol? spinner-ns) (nil? (find-ns spinner-ns)))
+             (create-ns spinner-ns)
+
+             (not (nil? spinner-ns))
+             spinner-ns
+
+             ;else: No spinner-ns set
+             :else
+             (create-ns 'spinner))]
+    (let [spin
+          (intern spinner-ns spinner-name
+            (if (fn? values)
+              values
+            ;else
+              (keyword->fnc spinner-name values fnc)))]
+      (swap! spinners assoc (str spinner-name) spin)
+      [spin values])))
+
+(defmacro defspin
+  ([spinner-name values]
+  `(binding [spinner-ns (or spinner-ns *ns*)]
+     (defspin* '~spinner-name ~values rand-nth)))
+  ([spinner-name values fnc]
+  `(binding [spinner-ns (or spinner-ns *ns*)]
+     (defspin* '~spinner-name ~values ~fnc))))
+
+(defn- spin-args
+  "Process 2-arity function arguments"
+  [file-or-name fnc-or-file action]
+  (if (symbol? file-or-name) ; file-or-name is a name, this fnc-or-file is a file
+    (action file-or-name fnc-or-file rand-nth) ; TODO: Make rand-nth configurable through dynamic binding
+    (action (symbol (basename file-or-name)) file-or-name fnc-or-file)))
+
+
+(defn spinfile
+  ([file] (spinfile file rand-nth))
+  ([file-or-name fnc-or-file]
+    (spin-args file-or-name fnc-or-file defspin*))
+  ([spinner-name file fnc]
+    (defspin* spinner-name file fnc)))
+
+(defn spindir
+  ([dir] (spindir dir rand-nth))
+  ([dir-or-name dir-or-fnc]
+    (spin-args dir-or-name dir-or-fnc spindir))
+  ([root-ns dir fnc]
+    (when (.isDirectory dir)
+      (binding [spinner-ns (or root-ns spinner-ns)]
+        (into (array-map)
+          (for [% (seq (.listFiles dir))]
+            (if (.isFile %)
+              (spinfile (symbol (basename %)) % fnc)
+              (spindir (symbol (str spinner-ns "." (.getName %))) % fnc))))))))
+
+(defn eval-spin [spinstr]
+  (-> spinstr compile-str apply-str))
---
+++ test/spinner/test.clj
@@ -0,0 +1,48 @@
+(ns spinner.test
+  (:require [clojure.java.io   :as io])
+  (:use spinner.core))
+
+;Binding test
+(binding [spinner-ns 'spinner]
+  ; TODO
+  ;  Namespace is mappenstructuur?
+  (spindir 'spinner (io/as-file "./resources") first))
+
+
+(refer 'spinner)
+(eval-spin "%a%")
+
+  ; File test
+  (binding [spinner/*test* "1996"]
+    (spinner/test->Price))
+  (spinner/a)
+  (spinner/blabla)
+  (spinner/english)
+
+; Map test
+(defspin macho (array-map
+                "macho"          ["man" "b" "c"]
+                "onder-de-plak"  ["mom" "dad" "scheit" "%macho%s"])
+  rand-nth)
+
+(spinner.test/macho->onder-de-plak)
+(binding [spinner.test/*macho* "man"]
+  (spinner.test/macho->onder-de-plak))
+(spinner.test/macho)
+
+; Seq test
+(defspin f ["last" "omega" "mega stupid"] rand-nth)
+(defspin mom ["%f%" "moeder"])
+(defspin child ["%mom%"])
+(parse-str "hi %mom%")
+(spinner.test/f)
+(spinner.test/mom)
+(spinner.test/child)
+
+;Function test
+(defspin fnc (fn [] "functie"))
+(spinner.test/fnc)
+
+(parse-str "%mom%")
+
+(@spinners "mom")
