c212a7c813912c725dde0fb914530a558b753e2a
Danny Wilson <danny@decube.net>
2015-03-16 15:11:53 +0100

    Project + maven

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject redshirtz/spinner "0.1.0-SNAPSHOT"
+  :description "Word spinning library"
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [ [org.clojure/clojure "1.5.1"]
+                  [clojure-csv "2.0.1"] ]
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.20"
+                 {:extensions "true"
+                  :configuration ([:sourceDirectories [:sourceDirectory "src"]]
+                                  [:temporaryOutputDirectory "true"])
+                  :executions [:execution
+                               [:id "compile-clojure"]
+                               [:phase "compile"]
+                               [:goals [:goal "compile"]]]}]])
