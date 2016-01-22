eb49e8a7e7d90ef30799c808a92fd67770429881
Danny Wilson <danny@prime.vc>
2016-01-22 23:03:11 +0100

    Update Clojure to 1.8.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject redshirtz/spinner "0.1.0-SNAPSHOT"
   :description "Word spinning library"
   :global-vars {*warn-on-reflection* true}
-  :dependencies [ [org.clojure/clojure "1.5.1"]
+  :dependencies [ [org.clojure/clojure "1.8.0"]
                   [clojure-csv "2.0.1"] ]
   :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.20"
                  {:extensions "true"
